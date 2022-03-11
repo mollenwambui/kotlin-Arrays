3a4a2b5e2724ee0c3851f94ac36ba161871cc678
mollenwambui <99389367+mollenwambui@users.noreply.github.com>
2022-03-11 13:12:05 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -21,7 +21,7 @@ fun students(a:String,b:String,c:String,d:String){
 }
 fun array(){
     var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
-    var sum=numbers[2]+numbers[5]
+    var sum=numbers[1]+numbers[4]
     var index= numbers.indexOf(158)
     var ascending=numbers.sorted()
     println(sum)
